a5611ca455e2f703434ed9c9f4614a0b0c1189c2
Paul Liesse <pliesse@matcmadison.edu>
2011-09-25 19:47:23 -0500

    Added named routes and new layout files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages2/home"
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
 
-  get "pages2/contact"
+  root  :to => 'pages#home'
 
-  get "pages/home"
-  get "pages/contact"
-  get "pages/about"
+end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -63,5 +63,4 @@ SampleApp::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
-end
 
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,14 @@
 require 'spec_helper'
 
+
 describe PagesController do
   render_views
+
+  before(:each) do
+    #
+    @base_title = "Sample App | "
+    #
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,7 +19,7 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                         :content => "Ruby on Rails Tutorial Sample App | Home")
+                         :content => @base_title + "Home")
     end
   end
 
@@ -24,7 +31,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                         :content => "Ruby on Rails Tutorial Sample App | Contact")
+                         :content => @base_title + "Contact")
     end
   end
 
@@ -36,7 +43,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                         :content => "Ruby on Rails Tutorial Sample App | About")
+                         :content => @base_title + "About")
     end
   end
 
